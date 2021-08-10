ca8944f54f5f7c59b3d22f00805f99c4a2793d4d
Tom Léost <tom@biglemon.co.uk>
2021-08-10 23:05:21 +0100

    Add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { ToolBarContextProvider }  from './contexts/ToolBarContext'
+
+Sentry.init({
+  dsn: process.env.SENTRY_URL,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
